9ed383ad3e5474382582a42a1c3a0b13446e7b97
user <user@DESKTOP-FBAEFCH>
2024-09-02 09:00:33 +0900

    2024/09/02 연우 고객센터

--- TJCamping/src/main/java/com/sist/model/ServiceCenterModel.java
+++ TJCamping/src/main/java/com/sist/model/ServiceCenterModel.java
@@ -16,12 +16,22 @@ import com.sist.dao.*;
 import com.sist.vo.*;
 
 public class ServiceCenterModel {
-
-	private String[] types= {"","일반공지","이벤트공지","맛집공지","여행공지"};
-
+	
 	@RequestMapping("servicecenter/servicecenter_main.do")
-	public String adminpage_main(HttpServletRequest request, HttpServletResponse response) {
-		// 공지사항
+	   public String servicecenter_list(HttpServletRequest request,HttpServletResponse response)
+	   {
+		   
+		   request.setAttribute("main_jsp", "../servicecenter/servicecenter_main.jsp");
+		   return "../main/main.jsp";
+	   }
+	
+	
+	
+
+private String[] types= {"","일반공지","이벤트공지"};
+	
+	@RequestMapping("servicecenter/camp_list.do")
+	public String camp_list(HttpServletRequest request , HttpServletResponse response) {
 		String page = request.getParameter("page");
 		if(page==null) page="1";
 		int curpage=Integer.parseInt(page);
@@ -32,26 +42,215 @@ public class ServiceCenterModel {
 		map.put("start", start);
 		map.put("end", end);
 		
-		List<NoticeVO> list = NoticeDAO.noticeListData(map);
-		for (NoticeVO vo : list) {
-			vo.setNotice_type(types[vo.getType()]);
+		List<ServiceVO> list = ServiceDAO.campBoardListData(map);
+		for (ServiceVO vo : list) {
+			vo.setCamp_type(types[vo.getType()]);
 		}
-		int count = NoticeDAO.noticeRowCount();
+		int count = ServiceDAO.campRowCount();
 		int totalpage=(int)(Math.ceil(count/15.0));
 		count = count-((curpage*rowSize)-rowSize);
 		
 		request.setAttribute("curpage", curpage);
 		request.setAttribute("totalpage", totalpage);
 		request.setAttribute("count", count);
-		request.setAttribute("noticeList", list);
-		
-		////////////////////////////////////////////////////////////////////////////////////
+		request.setAttribute("campBList", list);
 		
 		CommonsModel.footerPrint(request);
 		
+		request.setAttribute("main_jsp", "../servicecenter/camp_list.jsp");
+		return"../main/main.jsp";
+	}
+	// 상세보기
+	@RequestMapping("servicecenter/camp_detail.do")
+	public String notice_detail(HttpServletRequest request , HttpServletResponse response) {
 		
-		request.setAttribute("notice_jsp", "../notice/list.jsp");
-		request.setAttribute("main_jsp", "../servicecenter/servicecenter_main.jsp");
+		String no = request.getParameter("no");
+		ServiceVO vo = ServiceDAO.campDetailData(Integer.parseInt(no));
+		vo.setCamp_type(types[vo.getType()]);
+		
+		CommonsModel.footerPrint(request);
+		
+		request.setAttribute("vo", vo);
+		request.setAttribute("main_jsp", "../servicecenter/camp_detail.jsp");
 		return "../main/main.jsp";
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping("servicecenter/goods_list.do")
+	public String goods_list(HttpServletRequest request , HttpServletResponse response) {
+		String page = request.getParameter("page");
+		if(page==null) page="1";
+		int curpage=Integer.parseInt(page);
+		int rowSize=15;
+		int start=(rowSize*curpage)-(rowSize-1);
+		int end=(rowSize*curpage);
+		Map map = new HashMap();
+		map.put("start", start);
+		map.put("end", end);
+		
+		List<ServiceVO> list = ServiceDAO.goodsBoardListData(map);
+		for (ServiceVO vo : list) {
+			vo.setGoods_type(types[vo.getType()]);
+		}
+		int count = ServiceDAO.goodsRowCount();
+		int totalpage=(int)(Math.ceil(count/15.0));
+		count = count-((curpage*rowSize)-rowSize);
+		
+		request.setAttribute("curpage", curpage);
+		request.setAttribute("totalpage", totalpage);
+		request.setAttribute("count", count);
+		request.setAttribute("goodsList", list);
+		
+		CommonsModel.footerPrint(request);
+		
+		request.setAttribute("main_jsp", "../servicecenter/goods_list.jsp");
+		return"../main/main.jsp";
+	}
+	// 상세보기
+	@RequestMapping("servicecenter/goods_detail.do")
+	public String goods_detail(HttpServletRequest request , HttpServletResponse response) {
+		
+		String no = request.getParameter("no");
+		ServiceVO vo = ServiceDAO.goodsDetailData(Integer.parseInt(no));
+		vo.setGoods_type(types[vo.getType()]);
+		
+		CommonsModel.footerPrint(request);
+		
+		request.setAttribute("vo", vo);
+		request.setAttribute("main_jsp", "../servicecenter/goods_detail.jsp");
+		return "../main/main.jsp";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping("servicecenter/recipe_list.do")
+	public String recipe_list(HttpServletRequest request , HttpServletResponse response) {
+		String page = request.getParameter("page");
+		if(page==null) page="1";
+		int curpage=Integer.parseInt(page);
+		int rowSize=15;
+		int start=(rowSize*curpage)-(rowSize-1);
+		int end=(rowSize*curpage);
+		Map map = new HashMap();
+		map.put("start", start);
+		map.put("end", end);
+		
+		List<ServiceVO> list = ServiceDAO.recipeBoardListData(map);
+		for (ServiceVO vo : list) {
+			vo.setRecipe_type(types[vo.getType()]);
+		}
+		int count = ServiceDAO.recipeRowCount();
+		int totalpage=(int)(Math.ceil(count/15.0));
+		count = count-((curpage*rowSize)-rowSize);
+		
+		request.setAttribute("curpage", curpage);
+		request.setAttribute("totalpage", totalpage);
+		request.setAttribute("count", count);
+		request.setAttribute("recipeList", list);
+		
+		CommonsModel.footerPrint(request);
+		
+		request.setAttribute("main_jsp", "../servicecenter/recipe_list.jsp");
+		return"../main/main.jsp";
+	}
+	// 상세보기
+	@RequestMapping("servicecenter/recipe_detail.do")
+	public String recipe_detail(HttpServletRequest request , HttpServletResponse response) {
+		
+		String no = request.getParameter("no");
+		ServiceVO vo = ServiceDAO.recipeDetailData(Integer.parseInt(no));
+		vo.setRecipe_type(types[vo.getType()]);
+		
+		CommonsModel.footerPrint(request);
+		
+		request.setAttribute("vo", vo);
+		request.setAttribute("main_jsp", "../servicecenter/recipe_detail.jsp");
+		return "../main/main.jsp";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping("servicecenter/food_list.do")
+	public String food_list(HttpServletRequest request , HttpServletResponse response) {
+		String page = request.getParameter("page");
+		if(page==null) page="1";
+		int curpage=Integer.parseInt(page);
+		int rowSize=15;
+		int start=(rowSize*curpage)-(rowSize-1);
+		int end=(rowSize*curpage);
+		Map map = new HashMap();
+		map.put("start", start);
+		map.put("end", end);
+		
+		List<ServiceVO> list = ServiceDAO.foodBoardListData(map);
+		for (ServiceVO vo : list) {
+			vo.setFood_type(types[vo.getType()]);
+		}
+		int count = ServiceDAO.foodRowCount();
+		int totalpage=(int)(Math.ceil(count/15.0));
+		count = count-((curpage*rowSize)-rowSize);
+		
+		request.setAttribute("curpage", curpage);
+		request.setAttribute("totalpage", totalpage);
+		request.setAttribute("count", count);
+		request.setAttribute("foodList", list);
+		
+		CommonsModel.footerPrint(request);
+		
+		request.setAttribute("main_jsp", "../servicecenter/food_list.jsp");
+		return"../main/main.jsp";
+	}
+	// 상세보기
+	@RequestMapping("servicecenter/food_detail.do")
+	public String food_detail(HttpServletRequest request , HttpServletResponse response) {
+		
+		String no = request.getParameter("no");
+		ServiceVO vo = ServiceDAO.foodDetailData(Integer.parseInt(no));
+		vo.setFood_type(types[vo.getType()]);
+		
+		CommonsModel.footerPrint(request);
+		
+		request.setAttribute("vo", vo);
+		request.setAttribute("main_jsp", "../servicecenter/food_detail.jsp");
+		return "../main/main.jsp";
+	}
+	
+	
+	
+	
+	
+	
 }
